ee86c5b78fc412a77b7a1fc7a14a559dcba0f23e
guihaojin <guihaojin@gmail.com>
2012-09-18 23:30:30 -0700

    Update SearchInRotatedSortedArray.cpp

--- SearchInRotatedSortedArray.cpp
+++ SearchInRotatedSortedArray.cpp
@@ -3,6 +3,24 @@ public:
     int search(int A[], int n, int target) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-  		
+        int left = 0;
+        int right = n - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(A[mid] == target)
+                return mid;
+            else if(A[mid] > A[right]) { // the left half is sorted
+                if(target >= A[left] && target <= A[mid])
+                    right = mid -1;
+                else
+                    left = mid + 1;
+            } else { // right half is sorted
+                if(target >= A[mid] && target <= A[right])
+                    left = mid + 1;
+                else 
+                    right = mid - 1;
+            }
+        }
+        return -1;
     }
 };
